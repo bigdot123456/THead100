3d0cd64b3d5e8abf5f6ec1440de6b6dbfa633e64
李庆华 <liqh@certusnet.com.cn>
2022-06-09 20:55:54 +0800

    work correct for linux

--- runtest.sh
+++ runtest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd workdir
+mkdir -p workdir
 export TOOL_PATH=/eda/riscv/toolchain
+
 #../tools/run_case --sim_tool iverilog ../case/timer/timer_test.c
-../tools/run_case ../case/timer/timer_test.c
+./tools/run_case ./case/timer/timer_test.c
