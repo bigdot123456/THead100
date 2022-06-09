c64fbdd2506ade5b13780de4644530920f16feba
bigdot123456 <bigdot@126.com>
2022-06-09 20:09:27 +0800

    change vcs run

---
+++ runtest.cmd
@@ -0,0 +1,6 @@
+cd workdir
+rem export TOOL_PATH=/eda/riscv/toolchain
+rem ../tools/run_case --sim_tool iverilog ../case/timer/timer_test.c
+
+set TOOL_PATH=D:\riscv64-elf-mingw
+perl ..\tools\run_case ..\case\timer\timer_test.c
