2c077ef314bef120bb76c2ec87d2a90c2e4ed69e
bigdot123456 <bigdot@126.com>
2022-06-10 10:45:13 +0800

    add more debug info for cmd line

--- runtest.cmd
+++ runtest.cmd
@@ -1,6 +1,9 @@
+mkdir workdir
 cd workdir
 rem export TOOL_PATH=/eda/riscv/toolchain
 rem ../tools/run_case --sim_tool iverilog ../case/timer/timer_test.c
+rem D:\riscv64-elf-mingw\bin\riscv64-unknown-elf-gcc -Tlinker.lcf -nostartfiles -march=rv32emc -mabi=ilp32e -lc -lgcc  crt0.o __dtostr.o __isinf.o __isnan.o __lltostr.o __ltostr.o __v_printf.o fprintf.o fputc.o getc.o getchar.o printf.o putc.o putchar.o puts.o snprintf.o sprintf.o vfprintf.o vprintf.o vsnprintf.o vsprintf.o -o ..casetimertimer_test.elf -lm
 
-set TOOL_PATH=D:\riscv64-elf-mingw
-perl ..\tools\run_case ..\case\timer\timer_test.c
+set TOOL_PATH=D:/riscv64-elf-mingw
+perl ../tools/run_case --sim_tool iverilog ../case/timer/timer_test.c
+cd ..
--- runtest.sh
+++ runtest.sh
@@ -2,5 +2,6 @@
 mkdir -p workdir
 export TOOL_PATH=/eda/riscv/toolchain
 
-#../tools/run_case --sim_tool iverilog ../case/timer/timer_test.c
+# ./tools/run_case --sim_tool iverilog ./case/timer/timer_test.c
+# ../tools/run_case --sim_tool iverilog ../case/timer/timer_test.c
 ./tools/run_case ./case/timer/timer_test.c
