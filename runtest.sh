f872a255872aa48282b70a47e14390551eb20fca
bigdot123456 <bigdot@126.com>
2022-06-10 11:54:14 +0800

    change verdi home path

--- runtest.sh
+++ runtest.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set CASE=./case/timer/timer_test.c
+
 mkdir -p workdir
 export TOOL_PATH=/eda/riscv/toolchain
 
 # ./tools/run_case --sim_tool iverilog ./case/timer/timer_test.c
 # ../tools/run_case --sim_tool iverilog ../case/timer/timer_test.c
-./tools/run_case ./case/timer/timer_test.c
+./tools/run_case $CASE
